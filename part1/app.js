58f2886ebf915a4d8a21dcffd4281a31ce66d0dc
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 01:43:16 +0000

    [automatic commit] 2025-06-20 01:43:16 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -35,7 +35,13 @@ let db;
             database: 'DogWalkService'
         });
 
-        await db.execute
+        await db.execute(` CREATE TABLE IF NOT EXISTS Users (
+          user_id INT AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(50) UNIQUE NOT NULL,
+          email VARCHAR(100) UNIQUE NOT NULL,
+          password_hash VARCHAR(255) NOT NULL,
+          role ENUM('owner', 'walker') NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP`)
     }
 })
 
