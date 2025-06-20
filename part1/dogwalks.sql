0cdc8a496fad68e56f10ea9bfb8a3aa7c457f785
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 00:47:15 +0000

    [automatic commit] 2025-06-20 00:47:15 UTC 180.150.112.172

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,4 +58,4 @@ INSERT INTO Users (username , email, password_hash, role)
 VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner')
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-    ('carol')
+    ('carol123', 'carol@example.com')
