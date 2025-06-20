6ae6f59a275cab4495ffa3c549f135246c28e9e8
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 01:01:15 +0000

    [automatic commit] 2025-06-20 01:01:15 UTC 180.150.112.172

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -73,4 +73,4 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-    ((SELECT dog_id FROM Dogs WHERE name = 'Max'))
+    ((SELECT dog_id FROM Dogs WHERE name = 'Max',))
