f66c0c8699fe1c0e23cf78b996af99a002604d19
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 03:02:31 +0000

    [automatic commit] 2025-06-20 03:02:31 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -165,7 +165,7 @@ app.get('/api/walkers/summary', async (req, res) => {
         FROM WalkApplications wa
         JOIN WalkRequests wreq ON wa.request_id = wreq.request_id
         WHERE wa.status = 'accepted' AND wreq.status = 'completed'
-        GROUP BYwa.walker_id
+        GROUP BY wa.walker_id
         ) completed_walks ON u.user_id = completed_walks.walker_id
         WHERE u.role = 'walker'
         GROUP BY u.user_id, u.username, completed_walks.completed_count
