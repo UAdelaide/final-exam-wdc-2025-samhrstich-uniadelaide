526e042c0ce0d55f6fe930b37b6274efa9050a0a
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 02:22:18 +0000

    [automatic commit] 2025-06-20 02:22:18 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -131,7 +131,10 @@ app.get('/api/walkrequests/open', async (req, res) => {
         JOIN Dogs d ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status ='open'
-        ORDER BY wr.requested`
+        ORDER BY wr.requested_time
+    `;
+
+    const [rows]
     }
 })
 
