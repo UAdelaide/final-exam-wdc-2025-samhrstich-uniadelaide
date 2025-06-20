7cb31521b430d92ecc09028f018fc4045b81fd4a
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 01:45:19 +0000

    [automatic commit] 2025-06-20 01:45:19 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -46,7 +46,14 @@ let db;
         )
         `)
         await db.execute(`
-            `)
+            CREATE TABLE IF NOT EXISTS Dogs (
+          dog_id INT AUTO_INCREMENT PRIMARY KEY,
+          owner_id INT NOT NULL,
+          name VARCHAR(50) NOT NULL,
+          size ENUM('small', 'medium', 'large') NOT NULL,
+          FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+        )
+        `)
     }
 })
 
