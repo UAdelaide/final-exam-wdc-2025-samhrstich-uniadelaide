22f2fb260920e8a247a23eb571b7bc21e31c5e5b
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 01:31:18 +0000

    [automatic commit] 2025-06-20 01:31:18 UTC 180.150.112.172

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -54,27 +54,27 @@ CREATE TABLE WalkRatings (
     CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 );
 
-INSERT INTO Users (username , email, password_hash, role)
-VALUES
-    ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-    ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-    ('benjamin', 'benjamin@example.com', 'hashed868', 'owner'),
-    ('liam', 'liam@example.com', 'hashed687', 'owner'),
-    ('colepalmer', 'cole@example.com', 'hashed345', 'owner');
+-- INSERT INTO Users (username , email, password_hash, role)
+-- VALUES
+--     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+--     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+--     ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+--     ('benjamin', 'benjamin@example.com', 'hashed868', 'owner'),
+--     ('liam', 'liam@example.com', 'hashed687', 'owner'),
+--     ('colepalmer', 'cole@example.com', 'hashed345', 'owner');
 
-INSERT INTO Dogs (owner_id, name, size)
-VALUES
-    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-    ((SELECT user_id FROM Users WHERE username = 'benjamin'), 'Bailey', 'medium'),
-    ((SELECT user_id FROM Users WHERE username = 'liam'), 'Rufus', 'small'),
-    ((SELECT user_id FROM Users WHERE username = 'colepalmer'), 'Flop', 'medium');
+-- INSERT INTO Dogs (owner_id, name, size)
+-- VALUES
+--     ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+--     ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+--     ((SELECT user_id FROM Users WHERE username = 'benjamin'), 'Bailey', 'medium'),
+--     ((SELECT user_id FROM Users WHERE username = 'liam'), 'Rufus', 'small'),
+--     ((SELECT user_id FROM Users WHERE username = 'colepalmer'), 'Flop', 'medium');
 
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-VALUES
-    ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Bailey'), '2025-07-15 10:45:00', 25, 'Everest', 'accepted'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Rufus'), '2025-04-15 12:30:00', 30, 'Hindley Street', 'open'),
-    ((SELECT dog_id FROM Dogs WHERE name = 'Flop'), '2025-09-14 11:25:00', 35, 'Stamford Bridge', 'accepted');
+-- INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+-- VALUES
+--     ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+--     ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+--     ((SELECT dog_id FROM Dogs WHERE name = 'Bailey'), '2025-07-15 10:45:00', 25, 'Everest', 'accepted'),
+--     ((SELECT dog_id FROM Dogs WHERE name = 'Rufus'), '2025-04-15 12:30:00', 30, 'Hindley Street', 'open'),
+--     ((SELECT dog_id FROM Dogs WHERE name = 'Flop'), '2025-09-14 11:25:00', 35, 'Stamford Bridge', 'accepted');
