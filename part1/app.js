f58ad3c8154033740ece4b5132eb22b873db5723
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 02:05:52 +0000

    [automatic commit] 2025-06-20 02:05:52 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ let db;
             `);
             await db.execute(`
                 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-                (2, (SELECT))`)
+                (2, (SELECT user_id FROM Users WHERE username = 'bobwalker))`)
         }
     }
 })
