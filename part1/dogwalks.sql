6d6f6f1d76a61aa7d672f726dc42e7da6d98c4f8
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 00:46:54 +0000

    [automatic commit] 2025-06-20 00:46:54 UTC 180.150.112.172

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -57,4 +57,5 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users (username , email, password_hash, role)
 VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner')
-    ('bobwalker', 'bob@example.com', 'hashed')
+    ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
+    ('carol')
