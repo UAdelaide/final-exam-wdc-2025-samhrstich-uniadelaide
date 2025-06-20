d44adaf103e010f2bb0c5c183a094f8e790e2302
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-20 01:43:57 +0000

    [automatic commit] 2025-06-20 01:43:57 UTC 180.150.112.172

--- part1/app.js
+++ part1/app.js
@@ -42,7 +42,7 @@ let db;
           email VARCHAR(100) UNIQUE NOT NULL,
           password_hash VARCHAR(255) NOT NULL,
           role ENUM('owner', 'walker') NOT NULL,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP`)
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`)
     }
 })
 
