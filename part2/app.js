d4243a00b5cf2cd038996f7a34dfa91824132942
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-21 06:13:58 +0000

    [automatic commit] 2025-06-21 06:13:58 UTC 180.150.112.172

--- part2/app.js
+++ part2/app.js
@@ -12,65 +12,65 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-app.use(session({
-    secret: 'secretkey1234',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {secure: true}
-}));
+// app.use(session({
+//     secret: 'secretkey1234',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {secure: true}
+// }));
 
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+// app.post('/login', async (req, res) => {
+//     const { username, password } = req.body;
 
-    try {
-        const [rows] = await db.execute(
-            'SELECT user_id, username, email, password_hash, role FROM Users WHERE username =?',
-            [username]
-        );
-        if (rows.length ===0) {
-            return res.redirect('/?error=Invalid Username or Password');
-        }
-        const user = rows[0];
+//     try {
+//         const [rows] = await db.execute(
+//             'SELECT user_id, username, email, password_hash, role FROM Users WHERE username =?',
+//             [username]
+//         );
+//         if (rows.length ===0) {
+//             return res.redirect('/?error=Invalid Username or Password');
+//         }
+//         const user = rows[0];
 
-        if (user.password_hash !== password ) {
-            return res.redirect('/?error=Inavalid Username or password');
-        }
+//         if (user.password_hash !== password ) {
+//             return res.redirect('/?error=Inavalid Username or password');
+//         }
 
-        req.session.user_Id = user.user_id;
-        req.session.username = user.username;
-        req.session.role = user.role;
+//         req.session.user_Id = user.user_id;
+//         req.session.username = user.username;
+//         req.session.role = user.role;
 
-        if (user.role === 'owner') {
-            res.redirect('/owner-dashboard.html');
+//         if (user.role === 'owner') {
+//             res.redirect('/owner-dashboard.html');
 
-        } else if (user.role === 'walker') {
-            res.redirect('/walker-dashboard.html');
-        } else {
-            res.redirect('/?error=Invalid role');
-        }
-    } catch (error) {
-        console.error('Login error',error);
-        res.redirect('/?error=ServerError');
-    }
-});
+//         } else if (user.role === 'walker') {
+//             res.redirect('/walker-dashboard.html');
+//         } else {
+//             res.redirect('/?error=Invalid role');
+//         }
+//     } catch (error) {
+//         console.error('Login error',error);
+//         res.redirect('/?error=ServerError');
+//     }
+// });
 
-app.post('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.error('Logout erro:', err);
-        }
-        res.redirect('/');
-    });
-});
+// app.post('/logout', (req, res) => {
+//     req.session.destroy((err) => {
+//         if (err) {
+//             console.error('Logout erro:', err);
+//         }
+//         res.redirect('/');
+//     });
+// });
 
-const requireAuth = (req, res, next) => {
-    if (req.session.user_Id) {
-        next();
-    } else {
-        res.redirect('/?error=Please log in')
-    }
-};
+// const requireAuth = (req, res, next) => {
+//     if (req.session.user_Id) {
+//         next();
+//     } else {
+//         res.redirect('/?error=Please log in')
+//     }
+// };
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
