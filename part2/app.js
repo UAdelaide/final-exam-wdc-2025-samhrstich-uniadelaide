4853ec9e21e53c8011ebfece861c2a63c6f4384b
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-21 05:02:48 +0000

    [automatic commit] 2025-06-21 05:02:48 UTC 180.150.112.172

--- part2/app.js
+++ part2/app.js
@@ -57,7 +57,9 @@ app.post('/login', async (req, res) => {
 
 app.post('/logout', (req, res) => {
     req.session.destroy((err) => {
-        if (err)
+        if (err) {
+            console.error('Logout erro:', err);
+        }
     })
 })
 
