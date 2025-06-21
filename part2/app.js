40eb9c9dcec2f62374cf425f2f967f76e0adcf82
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-21 06:30:45 +0000

    [automatic commit] 2025-06-21 06:30:45 UTC 180.150.112.172

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-
+// cnf
 app.use(session({
     secret: 'secretkey1234',
     resave: false,
