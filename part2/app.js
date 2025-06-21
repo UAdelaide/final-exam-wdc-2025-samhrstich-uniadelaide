0d14f9823973213708d4c02dec1943741c5e1d3e
samhrstich-uniadelaide <samuel.hrstich@student.adelaide.edu.au>
2025-06-21 04:48:04 +0000

    [automatic commit] 2025-06-21 04:48:04 UTC 180.150.112.172

--- part2/app.js
+++ part2/app.js
@@ -8,8 +8,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express,)
 app.use(express.static(path.join(__dirname, '/public')));
-app.use()
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
